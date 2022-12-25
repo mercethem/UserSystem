6a55dbf1ae2ce9bb6566683571e7392b1f5101a3
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2022-12-25 22:49:56 +0300

    Access

---
+++ src/Access.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class Access {
+
+    public static void access() throws IOException {
+        System.out.println("Please enter want to access to account number");
+        Scanner keyboard = new Scanner(System.in);
+        long access = keyboard.nextLong();
+
+        if (access == Account.accountNumber) {
+            System.out.println("\nAccount Number : " + Account.accountNumber + "\nName : " + Account.name +
+                    "\nAddress : " + Account.address + "\nMail : " + Account.mail  +
+                    "\nPhone Number : " + Account.phoneNumber + "\nBirthday : " + Account.birthDay);
+
+        } else {
+            System.out.println("There isn't any customer as such as given account number.");
+            Menu.menu();
+        }
+
+    }
+}
