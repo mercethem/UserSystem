f8d79e464a53e2d9843fee2e1c10efa0ea92b7d7
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-15 17:56:14 +0300

    Created, initialized and connected with SQL system

--- src/Access.java
+++ src/Access.java
@@ -1,21 +1,44 @@
 import java.io.IOException;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Access {
-    public static void access() throws IOException {
-        System.out.println("Please enter want to access to account number");
+    public static void access() throws IOException, SQLException {
         Scanner keyboard = new Scanner(System.in);
-        long access = keyboard.nextLong();
 
-        if (access == Account.accountNumber) {
-            System.out.println("\nAccount Number : " + Account.accountNumber + "\nName : " + Account.name +
-                    "\nAddress : " + Account.address + "\nMail : " + Account.mail +
-                    "\nPhone Number : " + Account.phoneNumber + "\nBirthday : " + Account.birthDay);
+        Connection myConn = DriverManager.getConnection("jdbc:sqlserver://DESKTOP-T9F59R5\\MSSQLSERVER:1433;useSSL=false;encrypt=false;integratedSecurity=true;");
+        Statement myStmt = myConn.createStatement();
+        long accountNumber = 0;
+        String nameSurname = null;
+
+        System.out.println("How can you want to view ? \nAccount Number : 0 Name Surname : 1 ");
+        byte choose = keyboard.nextByte();
+        if (choose == 0) {
+            System.out.println("Please enter want to access to account number: ");
+            accountNumber = keyboard.nextLong();
+            ResultSet myQuery = myStmt.executeQuery("SELECT * FROM UserSystemAccountsStock.dbo.AccountsInformations WHERE AccountNumber = '" + accountNumber + "'");
+            while (myQuery.next()) {
+                System.out.printf("\nAccount Number: %s\nName Surname: %s\nAdress: %s\ne-mail: %s\nPhone Number: %s\nBirthday: %s\nBalance: %s \n", myQuery.getString("AccountNumber"), myQuery.getString("Name-Surname"),
+                        myQuery.getString("Address"), myQuery.getString("Mail"), myQuery.getString("PhoneNumber")
+                        , myQuery.getString("Birthday"), myQuery.getString("Balance"));
+            }
+            Menu.menu();
+
+        } else if (choose == 1) {
+            System.out.println("Please enter want to access to Name Surname: ");
+            nameSurname = keyboard.next();
+            ResultSet myQuery2 = myStmt.executeQuery("SELECT * FROM UserSystemAccountsStock.dbo.AccountsInformations WHERE [Name-Surname] LIKE '%" + nameSurname + "%'");
+            while (myQuery2.next()) {
+                System.out.printf("\nAccount Number: %s\nName Surname: %s\nAdress: %s\ne-mail: %s\nPhone Number: %s\nBirthday: %s\nBalance: %s \n", myQuery2.getString("AccountNumber"), myQuery2.getString("Name-Surname"),
+                        myQuery2.getString("Address"), myQuery2.getString("Mail"), myQuery2.getString("PhoneNumber")
+                        , myQuery2.getString("Birthday"), myQuery2.getString("Balance"));
+            }
+            Menu.menu();
 
         } else {
-            System.out.println("There isn't any customer as such as given account number.");
+            System.out.println("Please enter true data");
             Menu.menu();
         }
-
     }
 }
+
