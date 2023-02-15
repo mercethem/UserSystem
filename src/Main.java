3b188a0b3c8416d7adf1e20bfd6c8763fa4d5f4c
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-15 17:57:46 +0300

    Created, initialized and connected with SQL system

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.io.*;
+import java.sql.SQLException;
 import java.util.*;
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    //TODO set and get
+    public static void main(String[] args) throws IOException, SQLException {
         Menu.menu();
     }
 
