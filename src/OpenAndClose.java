2a1e24a89cbaf6b31a6b02119824117c663d5a9c
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-15 17:58:01 +0300

    Created, initialized and connected with SQL system

--- src/OpenAndClose.java
+++ src/OpenAndClose.java
@@ -1,57 +1,58 @@
-import java.io.*;
-import java.util.*;
-public class OpenAndClose {
-    public static void openAccount() throws IOException {
-        Account[] account = new Account[7];
-        account[1] = new Account(Account.accountNumber, Account.name, Account.address, Account.mail, Account.phoneNumber,
-                Account.birthDay, Account.balance);
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Please enter Account Number : ");
-        Account.accountNumber = keyboard.nextLong();
-        keyboard.nextLine();
-        System.out.println("Please enter name_surname (George_Sunshine) : ");
-        Account.name = keyboard.nextLine();
-        System.out.println("Please enter address : ");
-        Account.address = keyboard.nextLine();
-        System.out.println("Please enter mail : ");
-        Account.mail = keyboard.nextLine();
-        System.out.println("Please enter phone number (987_654_32_10) : ");
-        Account.phoneNumber = keyboard.nextLine();
-        System.out.println("Please enter your birth day (26_12_1990");
-        Account.birthDay = keyboard.nextLine();
-        Account.counter++;
-        Account.balance = 0;
-        WriteAndRead.Write();
-        Menu.menu();
+import java.io.IOException;
+import java.sql.*;
+import java.util.Scanner;
 
+public class OpenAndClose {
+    public static void openAccount() throws IOException, SQLException {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter Account Number for your new customer : ");
+        long accountNumber = keyboard.nextLong();
+        System.out.println("Please enter Name-Surname : ");
+        keyboard.nextLine();
+        String nameSurname = keyboard.nextLine();
+        System.out.println("Please enter Adress : ");
+        String address = keyboard.nextLine();
+        System.out.println("Please enter e-mail : ");
+        String mail = keyboard.next();
+        System.out.println("Please enter Phone Number : ");
+        long phoneNumber = keyboard.nextLong();
+        System.out.println("Please enter Birthday : ");
+        String birthday = keyboard.next();
+        System.out.println("Please enter balance : ");
+        long balance = keyboard.nextLong();
+        try {
+            Connection myConnection = DriverManager.getConnection("jdbc:sqlserver://DESKTOP-T9F59R5\\MSSQLSERVER:1433;useSSL=false;encrypt=false;integratedSecurity=true;");
+            Statement myStmt = myConnection.createStatement();
+            ResultSet myQuery = myStmt.executeQuery("INSERT INTO UserSystemAccountsStock.dbo.AccountsInformations(AccountNumber, [Name-Surname], [Address], [Mail], [PhoneNumber], [Birthday],[Balance]) " +
+                    "VALUES ('" + accountNumber + "' ,'" + nameSurname + "','" + address + "','" + mail + "','" + phoneNumber + "','" + birthday + "', '"+balance+"')");
+        }
+        //TODO eğer aynı accountNumber'de biri varsa if else kullanarak aktifleşmesini engelle
+        catch (Exception e){
+            System.out.println(e);
+        }
+        Menu.menu();
     }
 
-    public static void closeAccount() throws IOException {
+    public static void closeAccount() throws IOException, SQLException {
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please enter account number want to delete : ");
         long deleteAccountNumber = keyboard.nextLong();
-        if (Account.accountNumber == deleteAccountNumber) {
-            System.out.println("Are you sure want do DELETE the account ??? \nYes = 2 \nNo = 1 ");
-            byte answer = keyboard.nextByte();
-            switch (answer) {
-                case 1: {
-                    Menu.menu();
+        System.out.println("ARE YOU SURE WANT TO DELETE ??? \n NO : 0 \n YES : 1 ");
+        byte questionDelete = keyboard.nextByte();
 
-                }
-                case 2: {
-                    Account.accountNumber = Long.parseLong("Closed");
-                    Account.name = "Closed";
-                    Account.address = "Closed";
-                    Account.mail = "Closed";
-                    Account.phoneNumber = "Closed";
-                    Account.birthDay = "Closed";
-                    Account.balance = Long.parseLong("Closed");
-                    Account.counter--;
-
-                    break;
-                }
+        try {
+            if (questionDelete == 1) {
+                Connection myConnection = DriverManager.getConnection("jdbc:sqlserver://DESKTOP-T9F59R5\\MSSQLSERVER:1433;useSSL=false;encrypt=false;integratedSecurity=true;");
+                Statement myStmt = myConnection.createStatement();
+                ResultSet myQuery = myStmt.executeQuery(" DELETE FROM UserSystemAccountsStock.dbo.AccountsInformations WHERE AccountNumber =  '" + deleteAccountNumber + "' ");
+            } else if (questionDelete == 0) {
+                Menu.menu();
+            } else {
+                OpenAndClose.closeAccount();
             }
+        }catch (Exception e){
+            System.out.println(e);
         }
         Menu.menu();
     }
