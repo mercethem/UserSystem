91c2ab4c7c981d0ea3561afa0bca1c73a73c9dc6
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-18 16:23:54 +0300

    Reformatted and deleted comments

--- src/DBLayer.java
+++ src/DBLayer.java
@@ -5,14 +5,9 @@ import java.util.Scanner;
 
 public class DBLayer {
     public static void DataBase() {
-        //TODO (Fonksiyon olarak çalıştırmayı ve SQL kodunu başka fonksiyondan alarak burdan sadece bağlantı almayı yap.
         try {
             Connection myConnection = DriverManager.getConnection("jdbc:sqlserver://DESKTOP-T9F59R5\\MSSQLSERVER:1433;useSSL=false;encrypt=false;integratedSecurity=true;");
             Statement myStmt = myConnection.createStatement();
-            //ResultSet myQuery = myStmt.executeQuery("SELECT * FROM UserSystemAccountsStock.dbo.AccountsInformations WHERE [Name-Surname]='Ethem Merç'");
-          /*  while (myQuery.next()){
-              System.out.println(myQuery.getString("Address"));
-            }*/
         }
 
         catch (Exception e){
@@ -20,5 +15,4 @@ public class DBLayer {
 
         }
     }
-    //TODO
 }
