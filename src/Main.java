e9f26039fc6fd8458a3e28cd187df6f6b6524a42
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-02-18 16:24:07 +0300

    Reformatted and deleted comments

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.sql.SQLException;
 import java.util.*;
 public class Main {
-    //TODO set and get
     public static void main(String[] args) throws IOException, SQLException {
         Menu.menu();
     }
